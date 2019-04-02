ecacb4a3585d947a37cd2e12333f8180b35eb050
PanGargamel <pskiba37@gmail.com>
2019-04-02 22:35:30 +0200

    disabled displaying admin settings for users

--- app/src/main/java/pl/piotrskiba/angularowo/SettingsFragment.java
+++ app/src/main/java/pl/piotrskiba/angularowo/SettingsFragment.java
@@ -1,12 +1,22 @@
 package pl.piotrskiba.angularowo;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 
 import androidx.preference.PreferenceFragmentCompat;
+import pl.piotrskiba.angularowo.utils.RankUtils;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-        addPreferencesFromResource(R.xml.pref_settings);
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getContext());
+        String rank = sharedPreferences.getString(getString(R.string.pref_key_rank), null);
+        if(rank != null && RankUtils.isStaffRank(rank)){
+            addPreferencesFromResource(R.xml.pref_settings_admin);
+        }
+        else {
+            addPreferencesFromResource(R.xml.pref_settings);
+        }
     }
 }
