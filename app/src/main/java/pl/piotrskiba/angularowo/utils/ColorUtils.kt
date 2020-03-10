dcaabeadbebb072436f2501540ebeed057efefdb
piotrskibapl <pskiba37@gmail.com>
2020-03-10 13:23:49 +0100

    code documentation

--- app/src/main/java/pl/piotrskiba/angularowo/utils/ColorUtils.kt
+++ app/src/main/java/pl/piotrskiba/angularowo/utils/ColorUtils.kt
@@ -6,6 +6,15 @@ import androidx.core.content.ContextCompat
 import pl.piotrskiba.angularowo.R
 
 object ColorUtils {
+
+    /**
+     * A function used to change the brightness of the color
+     *
+     * @param color the color to have brightness changed
+     * @param factor brightness factor; factor > 1 will darken the color;
+     * factor < 1 will lighten the color
+     * @return the color with changed brightness
+     */
     @JvmStatic
     fun changeBrightness(color: Int, factor: Float): Int {
         val hsv = FloatArray(3)
@@ -16,6 +25,14 @@ object ColorUtils {
         return Color.HSVToColor(hsv)
     }
 
+    /**
+     * A function to change the Minecraft color code to an actual color
+     *
+     * @param context
+     * @param colorCode the color code to get color from
+     * @return color corresponding to the color code or color corresponding to the "7" color code
+     * if no corresponding color was found
+     */
     @JvmStatic
     fun getColorFromCode(context: Context, colorCode: String): Int {
         when (colorCode) {
