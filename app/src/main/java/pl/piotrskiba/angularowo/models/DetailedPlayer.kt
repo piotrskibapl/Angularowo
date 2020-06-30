ff6202960537c323f098ce784022cdf77433401e
pskiba37@gmail.com <vz7gLFH8imAf4t>
2020-06-30 21:24:55 +0200

    changed DetailedPlayer parameters order

--- app/src/main/java/pl/piotrskiba/angularowo/models/DetailedPlayer.kt
+++ app/src/main/java/pl/piotrskiba/angularowo/models/DetailedPlayer.kt
@@ -2,7 +2,7 @@ package pl.piotrskiba.angularowo.models
 
 import pl.piotrskiba.angularowo.utils.RankUtils
 
-class DetailedPlayer(val username: String, val uuid: String?, val status: String, private val rank: String, val isVanished: Boolean, val balance: Float, val playtime: Int, val tokens: Int) {
+class DetailedPlayer(val uuid: String?, val username: String, val status: String, private val rank: String, val isVanished: Boolean, val balance: Float, val playtime: Int, val tokens: Int) {
 
     fun getRank(): Rank {
         return RankUtils.getRankFromName(rank)
