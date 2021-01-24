c6d9b2f4a4db9919ad092a25e50b5f22456ac7de
piotrskibapl <pskiba37@gmail.com>
2021-01-24 22:51:02 +0100

    removed unused imports

--- app/src/main/java/pl/piotrskiba/angularowo/activities/ReportDetailsActivity.kt
+++ app/src/main/java/pl/piotrskiba/angularowo/activities/ReportDetailsActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.MenuItem
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import butterknife.BindView
 import butterknife.ButterKnife
