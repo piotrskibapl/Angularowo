e947373135db8f3b7a4d09805534a9b7cde98873
piotrskibapl <pskiba37@gmail.com>
2021-05-05 19:35:15 +0200

    add player avatar placeholder

--- app/src/main/java/pl/piotrskiba/angularowo/base/ui/databinding/ImageViewBindingAdapters.kt
+++ app/src/main/java/pl/piotrskiba/angularowo/base/ui/databinding/ImageViewBindingAdapters.kt
@@ -1,17 +1,23 @@
 package pl.piotrskiba.angularowo.base.ui.databinding
 
 import android.widget.ImageView
+import androidx.annotation.DrawableRes
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import pl.piotrskiba.angularowo.IntegerVersionSignature
 import pl.piotrskiba.angularowo.utils.GlideUtils
 
-@BindingAdapter("imageUrl")
-fun loadImage(view: ImageView, url: String?) {
-    if (!url.isNullOrEmpty()) {
+@BindingAdapter("imageUrl", "placeholderImage", requireAll = false)
+fun loadImage(
+    view: ImageView,
+    url: String,
+    @DrawableRes placeholder: Int?
+) {
+    if (url.isNotEmpty()) {
         Glide.with(view.context)
             .load(url)
             .signature(IntegerVersionSignature(GlideUtils.getSignatureVersionNumber(5)))
+            .placeholder(placeholder ?: 0)
             .into(view)
     }
 }
