f2848137eced466e8bf21bbbf23fa9e5a50a14bc
piotrskibapl <pskiba37@gmail.com>
2021-05-28 23:46:32 +0200

    applied player list sorting

--- domain/src/main/java/pl/piotrskiba/angularowo/domain/player/usecase/GetOnlinePlayerListUseCase.kt
+++ domain/src/main/java/pl/piotrskiba/angularowo/domain/player/usecase/GetOnlinePlayerListUseCase.kt
@@ -3,7 +3,7 @@ package pl.piotrskiba.angularowo.domain.player.usecase
 import io.reactivex.rxjava3.core.Single
 import pl.piotrskiba.angularowo.domain.player.model.Player
 import pl.piotrskiba.angularowo.domain.player.repository.PlayerRepository
-import pl.piotrskiba.angularowo.domain.rank.model.Rank
+import pl.piotrskiba.angularowo.domain.rank.model.Rank.UnknownRank
 import pl.piotrskiba.angularowo.domain.rank.model.toNewRank
 import pl.piotrskiba.angularowo.domain.rank.repository.RankRepository
 import javax.inject.Inject
@@ -19,11 +19,19 @@ class GetOnlinePlayerListUseCase @Inject constructor(
             .concatMap { playerList ->
                 rankRepository.getAllRanks().map { rankList ->
                     playerList.map { player ->
-                        val rankName = (player.rank as Rank.UnknownRank).name
-                        player.rank =
-                            rankList.firstOrNull { it.name == rankName } ?: rankName.toNewRank()
+                        val rankName = (player.rank as UnknownRank).name
+                        player.rank = rankList.firstOrNull { it.name == rankName } ?: rankName.toNewRank()
                         player
                     }
+                }
+            }
+            .concatMap { playerList ->
+                val sortedPlayerList: MutableList<Player> = mutableListOf()
+                rankRepository.getAllRanks().map { rankList ->
+                    rankList.forEach { rank ->
+                        sortedPlayerList.addAll(playerList.filter { it.rank.name == rank.name })
+                    }
+                    sortedPlayerList
                 }
             }
     }
