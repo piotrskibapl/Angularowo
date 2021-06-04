5c38dfaecfc43b5d3a3f2837466deb703f54ed94
piotrskibapl <pskiba37@gmail.com>
2021-06-04 21:21:09 +0200

    add universal error layout

--- app/src/main/java/pl/piotrskiba/angularowo/base/model/ViewModelState.kt
+++ app/src/main/java/pl/piotrskiba/angularowo/base/model/ViewModelState.kt
@@ -1,5 +1,8 @@
 package pl.piotrskiba.angularowo.base.model
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import pl.piotrskiba.angularowo.R
 import retrofit2.HttpException
 import java.io.IOException
 
@@ -9,7 +12,25 @@ sealed class ViewModelState {
     data class Error(val error: Throwable?) : ViewModelState()
 
     fun isLoading() = this is Loading
+
+    fun isAnyError() = this is Error
+
     fun isNetworkError() = this is Error && error is IOException
+
     fun isApiError() = this is Error && error is HttpException
-    fun isUnknownError() = this is Error && error !is IOException && error !is HttpException
+
+    @DrawableRes
+    fun errorImage() = when {
+        isNetworkError() -> R.drawable.ic_no_internet
+        isApiError() -> R.drawable.ic_error
+        else -> R.drawable.ic_mood_bad
+    }
+
+    @StringRes
+    fun errorText() = when {
+        isNetworkError() -> R.string.no_internet
+        isApiError() -> R.string.server_error
+        else -> R.string.unknown_error
+    }
 }
+
