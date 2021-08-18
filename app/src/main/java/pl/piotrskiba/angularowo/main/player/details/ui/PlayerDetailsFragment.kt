b6efcca4c13ea25bdb0940298c2a87cea4284584
piotrskibapl <pskiba37@gmail.com>
2021-08-18 23:48:07 +0200

    pass player to PlayerDetailsViewModel

--- app/src/main/java/pl/piotrskiba/angularowo/main/player/details/ui/PlayerDetailsFragment.kt
+++ app/src/main/java/pl/piotrskiba/angularowo/main/player/details/ui/PlayerDetailsFragment.kt
@@ -7,26 +7,26 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.ViewModelProvider
-import pl.piotrskiba.angularowo.AppViewModel
 import pl.piotrskiba.angularowo.Constants
 import pl.piotrskiba.angularowo.R
+import pl.piotrskiba.angularowo.base.di.obtainViewModel
 import pl.piotrskiba.angularowo.base.ui.BaseFragment
 import pl.piotrskiba.angularowo.databinding.FragmentPlayerDetailsBinding
+import pl.piotrskiba.angularowo.main.base.viewmodel.MainViewModel
 import pl.piotrskiba.angularowo.main.player.details.viewmodel.PlayerDetailsViewModel
 import pl.piotrskiba.angularowo.main.player.model.PlayerBannerData
 import pl.piotrskiba.angularowo.utils.PreferenceUtils
 
 class PlayerDetailsFragment : BaseFragment<PlayerDetailsViewModel>(PlayerDetailsViewModel::class) {
 
+    private lateinit var mainViewModel: MainViewModel
     private lateinit var preferenceUtils: PreferenceUtils
 
-    private lateinit var mViewModel: AppViewModel
-
     override fun onCreate(savedInstanceState: Bundle?) {
-        mViewModel = ViewModelProvider(this).get(AppViewModel::class.java)
+        mainViewModel = viewModelFactory.obtainViewModel(requireActivity())
         preferenceUtils = PreferenceUtils(requireActivity())
         loadArguments()
+        observePlayer()
         super.onCreate(savedInstanceState)
     }
 
@@ -44,6 +44,12 @@ class PlayerDetailsFragment : BaseFragment<PlayerDetailsViewModel>(PlayerDetails
         actionbar?.setDisplayShowHomeEnabled(true)
 
         return view
+    }
+
+    private fun observePlayer() {
+        mainViewModel.player.observe(viewLifecycleOwner, {
+            viewModel.player = it
+        })
     }
 
     private fun loadArguments() {
--- app/src/main/java/pl/piotrskiba/angularowo/main/player/details/viewmodel/PlayerDetailsViewModel.kt
+++ app/src/main/java/pl/piotrskiba/angularowo/main/player/details/viewmodel/PlayerDetailsViewModel.kt
@@ -1,9 +1,12 @@
 package pl.piotrskiba.angularowo.main.player.details.viewmodel
 
 import pl.piotrskiba.angularowo.base.viewmodel.LifecycleViewModel
+import pl.piotrskiba.angularowo.domain.player.model.DetailedPlayer
 import pl.piotrskiba.angularowo.main.player.model.PlayerBannerData
 import javax.inject.Inject
 
 class PlayerDetailsViewModel @Inject constructor() : LifecycleViewModel() {
+
+    lateinit var player: DetailedPlayer
     lateinit var previewedPlayer: PlayerBannerData
 }
