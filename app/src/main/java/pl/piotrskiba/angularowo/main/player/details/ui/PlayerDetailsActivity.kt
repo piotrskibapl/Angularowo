26ce1c645c829f7fa218999c368e881357fc6c9c
piotrskibapl <pskiba37@gmail.com>
2021-08-18 20:58:51 +0200

    implement toolbar in PlayerDetailsActivity

--- app/src/main/java/pl/piotrskiba/angularowo/main/player/details/ui/PlayerDetailsActivity.kt
+++ app/src/main/java/pl/piotrskiba/angularowo/main/player/details/ui/PlayerDetailsActivity.kt
@@ -1,6 +1,10 @@
 package pl.piotrskiba.angularowo.main.player.details.ui
 
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import butterknife.BindView
+import butterknife.ButterKnife
 import pl.piotrskiba.angularowo.Constants
 import pl.piotrskiba.angularowo.R
 import pl.piotrskiba.angularowo.base.ui.BaseActivity
@@ -8,17 +12,37 @@ import pl.piotrskiba.angularowo.main.player.model.PlayerBannerData
 
 class PlayerDetailsActivity : BaseActivity() {
 
+    @BindView(R.id.toolbar)
+    lateinit var mToolbar: Toolbar
+
     private lateinit var previewedPlayer: PlayerBannerData
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ButterKnife.bind(this)
         loadArguments()
+        setupToolbar()
         setupMainFragment()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        android.R.id.home -> {
+            supportFinishAfterTransition()
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
     }
 
     private fun loadArguments() {
         previewedPlayer = intent.getSerializableExtra(Constants.EXTRA_PREVIEWED_PLAYER) as PlayerBannerData
+    }
+
+    private fun setupToolbar() {
+        setSupportActionBar(mToolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        supportActionBar?.setTitle(R.string.player_info)
     }
 
     private fun setupMainFragment() {
