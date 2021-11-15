879a76e663e249f273b38856364def8832519a25
piotrskibapl <pskiba37@gmail.com>
2021-11-15 03:38:25 +0100

    trim punishment reason in banners

--- app/src/main/java/pl/piotrskiba/angularowo/main/punishment/model/PunishmentBannerData.kt
+++ app/src/main/java/pl/piotrskiba/angularowo/main/punishment/model/PunishmentBannerData.kt
@@ -7,14 +7,26 @@ import pl.piotrskiba.angularowo.domain.punishment.model.PunishmentModel
 import pl.piotrskiba.angularowo.domain.punishment.model.PunishmentType
 import pl.piotrskiba.angularowo.utils.UrlUtils
 import java.io.Serializable
+import java.util.Locale
 
 data class PunishmentBannerData(
     val id: String,
     val uuid: String?,
     val username: String,
-    val reason: String,
+    private val reason: String,
     private val punishmentType: PunishmentType,
 ) : Serializable {
+
+    fun reason(context: Context): String {
+        val reasonTrimDelimiters = arrayOf(":", " (")
+        val trimmedReason = reason.split(*reasonTrimDelimiters).first().toLowerCase(Locale.getDefault())
+        return when (punishmentType) {
+            PunishmentType.BAN -> context.getString(R.string.punishment_ban_description_format, trimmedReason)
+            PunishmentType.MUTE -> context.getString(R.string.punishment_mute_description_format, trimmedReason)
+            PunishmentType.WARN -> context.getString(R.string.punishment_warn_description_format, trimmedReason)
+            PunishmentType.KICK -> context.getString(R.string.punishment_kick_description_format, trimmedReason)
+        }
+    }
 
     fun userAvatarUrl(context: Context) = UrlUtils.buildAvatarUrl(uuid, true, context)
 
