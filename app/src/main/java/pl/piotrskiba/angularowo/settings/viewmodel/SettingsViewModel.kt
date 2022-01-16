95f4e19bcfc2d9d79e0f3e3421b962624ad1c656
Piotr Skiba <pskiba37@gmail.com>
2022-01-16 02:20:32 +0100

    connect settings items checkboxes with viewmodel

--- app/src/main/java/pl/piotrskiba/angularowo/settings/viewmodel/SettingsViewModel.kt
+++ app/src/main/java/pl/piotrskiba/angularowo/settings/viewmodel/SettingsViewModel.kt
@@ -1,11 +1,32 @@
 package pl.piotrskiba.angularowo.settings.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import javax.inject.Inject
 
 class SettingsViewModel @Inject constructor() : ViewModel() {
 
-    fun onSettingsItemClicked() {
-        // TODO: handle item clicks
+    val eventsChecked = MutableLiveData(false)
+    val privateMessagesChecked = MutableLiveData(false)
+    val accountIncidentsChecked = MutableLiveData(false)
+    val newReportsChecked = MutableLiveData(false)
+
+    fun onEventsNotificationsClicked() {
+        eventsChecked.apply { value = !value!! }
+    }
+
+    fun onPrivateMessagesNotificationsClicked() {
+        privateMessagesChecked.apply { value = !value!! }
+    }
+
+    fun onAccountIncidentsNotificationsClicked() {
+        accountIncidentsChecked.apply { value = !value!! }
+    }
+
+    fun onNewReportsNotificationsClicked() {
+        newReportsChecked.apply { value = !value!! }
+    }
+
+    fun onLogoutClicked() {
     }
 }
