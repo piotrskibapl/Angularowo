9727b9e4975f4b9c23d584badf372fdb0e564a80
Piotr Skiba <pskiba37@gmail.com>
2022-01-16 01:51:56 +0100

    add basic items to settings view

--- app/src/main/java/pl/piotrskiba/angularowo/settings/viewmodel/SettingsViewModel.kt
+++ app/src/main/java/pl/piotrskiba/angularowo/settings/viewmodel/SettingsViewModel.kt
@@ -3,4 +3,9 @@ package pl.piotrskiba.angularowo.settings.viewmodel
 import androidx.lifecycle.ViewModel
 import javax.inject.Inject
 
-class SettingsViewModel @Inject constructor() : ViewModel()
+class SettingsViewModel @Inject constructor() : ViewModel() {
+
+    fun onSettingsItemClicked() {
+        // TODO: handle item clicks
+    }
+}
