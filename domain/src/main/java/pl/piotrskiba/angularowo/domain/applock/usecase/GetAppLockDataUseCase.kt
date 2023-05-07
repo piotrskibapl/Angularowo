9a0f16bfb65cda5cc3a588a44ed8f007e1147e18
Piotr Skiba <pskiba37@gmail.com>
2023-05-08 00:28:27 +0200

    fix applock crashing for not logged in users

--- domain/src/main/java/pl/piotrskiba/angularowo/domain/applock/usecase/GetAppLockDataUseCase.kt
+++ domain/src/main/java/pl/piotrskiba/angularowo/domain/applock/usecase/GetAppLockDataUseCase.kt
@@ -1,5 +1,6 @@
 package pl.piotrskiba.angularowo.domain.applock.usecase
 
+import io.reactivex.rxjava3.core.Single
 import pl.piotrskiba.angularowo.domain.applock.model.AppLockDataModel
 import pl.piotrskiba.angularowo.domain.applock.repository.AppLockRepository
 import pl.piotrskiba.angularowo.domain.base.preferences.repository.PreferencesRepository
@@ -24,6 +25,10 @@ class GetAppLockDataUseCase @Inject constructor(
             }
 
     private fun checkIgnoreAppLockPermission() =
-        playerRepository.getPlayerDetailsFromUuid(preferencesRepository.accessToken!!, preferencesRepository.uuid!!)
-            .map { it.permissions.contains(PermissionModel.IGNORE_APP_LOCK) }
+        if (preferencesRepository.accessToken != null && preferencesRepository.uuid != null) {
+            playerRepository.getPlayerDetailsFromUuid(preferencesRepository.accessToken!!, preferencesRepository.uuid!!)
+                .map { it.permissions.contains(PermissionModel.IGNORE_APP_LOCK) }
+        } else {
+            Single.just(false)
+        }
 }
