2e0b143f30c27e8f68d5c711baae5b0f1beb3ecd
piotr.skiba <piotr.skiba@intive.com>
2023-05-24 13:28:32 +0200

    fix back button navigation

---
+++ app/src/main/java/pl/piotrskiba/angularowo/main/base/model/NavigationComponent.kt
@@ -0,0 +1,16 @@
+package pl.piotrskiba.angularowo.main.base.model
+
+import pl.piotrskiba.angularowo.R
+
+object NavigationComponent {
+
+    val topLevelDestinations =
+        setOf(
+            R.id.mainScreenFragment,
+            R.id.playerListFragment,
+            R.id.chatFragment,
+            R.id.lastPunishmentsFragment,
+            R.id.offersFragment,
+            R.id.reportListContainerFragment,
+        )
+}
--- app/src/main/java/pl/piotrskiba/angularowo/main/base/ui/MainActivity.kt
+++ app/src/main/java/pl/piotrskiba/angularowo/main/base/ui/MainActivity.kt
@@ -23,6 +23,7 @@ import pl.piotrskiba.angularowo.applock.ui.AppLockActivity
 import pl.piotrskiba.angularowo.base.ui.BaseActivity
 import pl.piotrskiba.angularowo.databinding.ActivityMainBinding
 import pl.piotrskiba.angularowo.login.ui.LoginActivity
+import pl.piotrskiba.angularowo.main.base.model.NavigationComponent
 import pl.piotrskiba.angularowo.main.base.nav.MainNavigator
 import pl.piotrskiba.angularowo.main.base.viewmodel.MainViewModel
 import pl.piotrskiba.angularowo.utils.NotificationUtils
@@ -46,19 +47,18 @@ class MainActivity : BaseActivity<MainViewModel>(MainViewModel::class), MainNavi
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
         val drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.mainScreenFragment,
-                R.id.playerListFragment,
-                R.id.chatFragment,
-                R.id.lastPunishmentsFragment,
-                R.id.offersFragment,
-                R.id.reportListContainerFragment
-            ), drawerLayout
-        )
-        findViewById<Toolbar>(R.id.toolbar).setupWithNavController(navController, appBarConfiguration)
+        val appBarConfiguration = AppBarConfiguration(NavigationComponent.topLevelDestinations, drawerLayout)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        toolbar.setupWithNavController(navController, appBarConfiguration)
         findViewById<NavigationView>(R.id.nav_view).setupWithNavController(navController)
-        setSupportActionBar(findViewById(R.id.toolbar))
+        setSupportActionBar(toolbar)
+        toolbar.setNavigationOnClickListener {
+            if (navController.currentDestination?.id in NavigationComponent.topLevelDestinations) {
+                drawerLayout.open()
+            } else {
+                navController.navigateUp()
+            }
+        }
 
         preferenceUtils = PreferenceUtils(this)
 
