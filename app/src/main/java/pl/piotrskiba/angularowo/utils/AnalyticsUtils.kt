396d8e90f42d173ae1d5e92dadd3e3ffc6d64039
piotr.skiba <piotr.skiba@intive.com>
2023-05-25 15:53:10 +0200

    drop unused analytics functions

--- app/src/main/java/pl/piotrskiba/angularowo/utils/AnalyticsUtils.kt
+++ app/src/main/java/pl/piotrskiba/angularowo/utils/AnalyticsUtils.kt
@@ -9,22 +9,11 @@ private const val PLAYER_UUID = "player_uuid"
 private const val PLAYER_NAME = "player_name"
 private const val TARGET_UUID = "target_uuid"
 private const val TARGET_NAME = "target_name"
-private const val AD_OFFER_ID = "ad_offer_id"
-private const val OFFER_ID = "offer_id"
 
 private const val EVENT_LOGIN = "login_success"
 private const val EVENT_LOGIN_ERROR = "login_failed"
-private const val EVENT_LOGOUT_DIALOG_OPEN = "logout_dialog_open"
-private const val EVENT_LOGOUT_PROCEED = "logout_proceed"
-private const val EVENT_LOGOUT_CANCEL = "logout_cancel"
 private const val EVENT_FAVORITE = "action_favorite"
 private const val EVENT_UNFAVORITE = "action_unfavorite"
-private const val EVENT_AD_OFFER_DIALOG_OPEN = "ad_offer_dialog_open"
-private const val EVENT_AD_OFFER_PROCEED = "ad_offer_proceed"
-private const val EVENT_AD_OFFER_CANCEL = "ad_offer_cancel"
-private const val EVENT_OFFER_DIALOG_OPEN = "offer_dialog_open"
-private const val EVENT_OFFER_PROCEED = "offer_proceed"
-private const val EVENT_OFFER_CANCEL = "offer_cancel"
 private const val EVENT_ERROR_MESSAGE = "error_message"
 
 class AnalyticsUtils {
@@ -42,27 +31,6 @@ class AnalyticsUtils {
         Firebase.analytics.logEvent(EVENT_LOGIN_ERROR, bundle)
     }
 
-    fun logLogoutDialogOpen(playerUuid: String, playerName: String) {
-        val bundle = Bundle()
-        bundle.putString(PLAYER_UUID, playerUuid)
-        bundle.putString(PLAYER_NAME, playerName)
-        Firebase.analytics.logEvent(EVENT_LOGOUT_DIALOG_OPEN, bundle)
-    }
-
-    fun logLogoutProceed(playerUuid: String, playerName: String) {
-        val bundle = Bundle()
-        bundle.putString(PLAYER_UUID, playerUuid)
-        bundle.putString(PLAYER_NAME, playerName)
-        Firebase.analytics.logEvent(EVENT_LOGOUT_PROCEED, bundle)
-    }
-
-    fun logLogoutCancel(playerUuid: String, playerName: String) {
-        val bundle = Bundle()
-        bundle.putString(PLAYER_UUID, playerUuid)
-        bundle.putString(PLAYER_NAME, playerName)
-        Firebase.analytics.logEvent(EVENT_LOGOUT_CANCEL, bundle)
-    }
-
     fun logScreenView(screenClass: String, screenName: String) {
         val bundle = Bundle()
         bundle.putString(FirebaseAnalytics.Param.SCREEN_CLASS, screenClass)
@@ -70,6 +38,7 @@ class AnalyticsUtils {
         Firebase.analytics.logEvent(FirebaseAnalytics.Event.SCREEN_VIEW, bundle)
     }
 
+    // TODO: track favorite/unfavorite actions
     fun logFavorite(playerUuid: String, playerName: String, targetUuid: String, targetName: String) {
         val bundle = Bundle()
         bundle.putString(PLAYER_UUID, playerUuid)
@@ -86,53 +55,5 @@ class AnalyticsUtils {
         bundle.putString(TARGET_UUID, targetUuid)
         bundle.putString(TARGET_NAME, targetName)
         Firebase.analytics.logEvent(EVENT_UNFAVORITE, bundle)
-    }
-
-    fun logAdOfferDialogOpen(playerUuid: String, playerName: String, adOfferId: String) {
-        val bundle = Bundle()
-        bundle.putString(PLAYER_UUID, playerUuid)
-        bundle.putString(PLAYER_NAME, playerName)
-        bundle.putString(AD_OFFER_ID, adOfferId)
-        Firebase.analytics.logEvent(EVENT_AD_OFFER_DIALOG_OPEN, bundle)
-    }
-
-    fun logAdOfferProceed(playerUuid: String, playerName: String, adOfferId: String) {
-        val bundle = Bundle()
-        bundle.putString(PLAYER_UUID, playerUuid)
-        bundle.putString(PLAYER_NAME, playerName)
-        bundle.putString(AD_OFFER_ID, adOfferId)
-        Firebase.analytics.logEvent(EVENT_AD_OFFER_PROCEED, bundle)
-    }
-
-    fun logAdOfferCancel(playerUuid: String, playerName: String, adOfferId: String) {
-        val bundle = Bundle()
-        bundle.putString(PLAYER_UUID, playerUuid)
-        bundle.putString(PLAYER_NAME, playerName)
-        bundle.putString(AD_OFFER_ID, adOfferId)
-        Firebase.analytics.logEvent(EVENT_AD_OFFER_CANCEL, bundle)
-    }
-
-    fun logOfferDialogOpen(playerUuid: String, playerName: String, offerId: String) {
-        val bundle = Bundle()
-        bundle.putString(PLAYER_UUID, playerUuid)
-        bundle.putString(PLAYER_NAME, playerName)
-        bundle.putString(AD_OFFER_ID, offerId)
-        Firebase.analytics.logEvent(EVENT_OFFER_DIALOG_OPEN, bundle)
-    }
-
-    fun logOfferProceed(playerUuid: String, playerName: String, offerId: String) {
-        val bundle = Bundle()
-        bundle.putString(PLAYER_UUID, playerUuid)
-        bundle.putString(PLAYER_NAME, playerName)
-        bundle.putString(AD_OFFER_ID, offerId)
-        Firebase.analytics.logEvent(EVENT_OFFER_PROCEED, bundle)
-    }
-
-    fun logOfferCancel(playerUuid: String, playerName: String, offerId: String) {
-        val bundle = Bundle()
-        bundle.putString(PLAYER_UUID, playerUuid)
-        bundle.putString(PLAYER_NAME, playerName)
-        bundle.putString(AD_OFFER_ID, offerId)
-        Firebase.analytics.logEvent(EVENT_OFFER_CANCEL, bundle)
     }
 }
