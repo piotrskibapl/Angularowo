1417b0e659a27fa941bf4be3352f13a9f35267db
piotr.skiba <piotr.skiba@intive.com>
2023-05-25 18:58:22 +0200

    handle UndeliverableException errors

--- app/src/main/java/pl/piotrskiba/angularowo/base/AngularowoApplication.kt
+++ app/src/main/java/pl/piotrskiba/angularowo/base/AngularowoApplication.kt
@@ -1,13 +1,16 @@
 package pl.piotrskiba.angularowo.base
 
 import android.content.Context
+import android.util.Log
 import androidx.multidex.MultiDex
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
+import io.reactivex.rxjava3.exceptions.UndeliverableException
+import io.reactivex.rxjava3.plugins.RxJavaPlugins
 import pl.piotrskiba.angularowo.base.di.AppComponent
 import pl.piotrskiba.angularowo.base.di.DaggerAppComponent
 
-class AngularowoApplication: DaggerApplication() {
+class AngularowoApplication : DaggerApplication() {
 
     private lateinit var appComponent: AppComponent
 
@@ -21,5 +24,20 @@ class AngularowoApplication: DaggerApplication() {
             .application(this)
             .build()
         return appComponent
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        setupRxErrorHandler()
+    }
+
+    private fun setupRxErrorHandler() {
+        RxJavaPlugins.setErrorHandler { error ->
+            if (error !is UndeliverableException) {
+                Thread.currentThread().uncaughtExceptionHandler?.uncaughtException(Thread.currentThread(), error)
+            } else {
+                Log.w(javaClass.name, "Undeliverable exception received: $error")
+            }
+        }
     }
 }
