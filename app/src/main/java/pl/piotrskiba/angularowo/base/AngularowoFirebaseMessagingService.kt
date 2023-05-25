d2d35ff19cee631bba760b01ba923e8a0e4a7a12
piotr.skiba <piotr.skiba@intive.com>
2023-05-25 19:47:54 +0200

    rename MyFirebaseMessagingService to AngularowoFirebaseMessagingService

--- app/src/main/java/pl/piotrskiba/angularowo/base/AngularowoFirebaseMessagingService.kt
+++ app/src/main/java/pl/piotrskiba/angularowo/base/AngularowoFirebaseMessagingService.kt
@@ -1,26 +1,24 @@
-package pl.piotrskiba.angularowo
+package pl.piotrskiba.angularowo.base
 
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import pl.piotrskiba.angularowo.Constants
 import pl.piotrskiba.angularowo.utils.NotificationUtils
 import pl.piotrskiba.angularowo.utils.PreferenceUtils
 
-class MyFirebaseMessagingService : FirebaseMessagingService() {
+class AngularowoFirebaseMessagingService : FirebaseMessagingService() {
+
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         super.onMessageReceived(remoteMessage)
+        // TODO: drop PreferenceUtils
         val preferenceUtils = PreferenceUtils(applicationContext)
-
-        // show notifications to logged in users only
         if (preferenceUtils.uuid != null) {
             val notificationBody = remoteMessage.data[Constants.FIREBASE_FCM_DATA_NOTIFICATION_BODY]
-
-            notificationBody?.run {
+            if (notificationBody != null) {
                 val notificationTitle = remoteMessage.data[Constants.FIREBASE_FCM_DATA_NOTIFICATION_TITLE]
                 val notificationSound = remoteMessage.data[Constants.FIREBASE_FCM_DATA_NOTIFICATION_SOUND]?.toBoolean()
-
                 if (notificationTitle != null && notificationSound != null) {
-                    NotificationUtils(applicationContext)
-                            .showNotification(notificationTitle, notificationBody, notificationSound)
+                    NotificationUtils(applicationContext).showNotification(notificationTitle, notificationBody, notificationSound)
                 }
             }
         }
