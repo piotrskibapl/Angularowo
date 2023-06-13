acc65d1d24d90c731a06f2b9c74d2df2588acf1d
piotr.skiba <piotr.skiba@intive.com>
2023-06-13 13:55:18 +0200

    fix ktlint domain module issues

--- domain/src/main/java/pl/piotrskiba/angularowo/domain/login/model/AccessTokenModel.kt
+++ domain/src/main/java/pl/piotrskiba/angularowo/domain/login/model/AccessTokenModel.kt
@@ -1,8 +1,8 @@
 package pl.piotrskiba.angularowo.domain.login.model
 
 data class AccessTokenModel(
-        val uuid: String,
-        val username: String,
-        val accessToken: String,
-        val message: String,
+    val uuid: String,
+    val username: String,
+    val accessToken: String,
+    val message: String,
 )
