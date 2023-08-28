10b922b904183c47718c297412ac19d7f9397be6
piotr.skiba <piotr.skiba@intive.com>
2023-08-28 12:54:41 +0200

    drop ColorUtils

--- app/src/main/java/pl/piotrskiba/angularowo/main/chat/model/ChatMessage.kt
+++ app/src/main/java/pl/piotrskiba/angularowo/main/chat/model/ChatMessage.kt
@@ -1,12 +1,13 @@
 package pl.piotrskiba.angularowo.main.chat.model
 
 import android.content.Context
+import android.graphics.Color
 import android.text.Spanned
+import androidx.core.content.ContextCompat
 import androidx.core.text.HtmlCompat
 import pl.piotrskiba.angularowo.R
 import pl.piotrskiba.angularowo.domain.chat.model.ChatMessageModel
 import pl.piotrskiba.angularowo.domain.rank.model.RankModel
-import pl.piotrskiba.angularowo.utils.ColorUtils
 import pl.piotrskiba.angularowo.utils.TextUtils
 
 data class ChatMessage(
@@ -16,25 +17,36 @@ data class ChatMessage(
     val message: String,
 ) {
 
+    private val colorCodesMap = mapOf(
+        "0" to R.color.color_minecraft_0,
+        "1" to R.color.color_minecraft_1,
+        "2" to R.color.color_minecraft_2,
+        "3" to R.color.color_minecraft_3,
+        "4" to R.color.color_minecraft_4,
+        "5" to R.color.color_minecraft_5,
+        "6" to R.color.color_minecraft_6,
+        "7" to R.color.color_minecraft_7,
+        "8" to R.color.color_minecraft_8,
+        "9" to R.color.color_minecraft_9,
+        "a" to R.color.color_minecraft_a,
+        "b" to R.color.color_minecraft_b,
+        "c" to R.color.color_minecraft_c,
+        "d" to R.color.color_minecraft_d,
+        "e" to R.color.color_minecraft_e,
+        "f" to R.color.color_minecraft_f,
+    )
+
     fun formattedMessage(context: Context): Spanned {
         val rankColor = String.format(
             "#%06X",
-            0xFFFFFF and ColorUtils.changeBrightness(
-                ColorUtils.getColorFromCode(
-                    context,
-                    rank.colorCode,
-                ),
-                1.4f,
+            0xFFFFFF and darkenColor(
+                ContextCompat.getColor(context, colorCodesMap[rank.colorCode] ?: 7),
             ),
         )
         val messageColor = String.format(
             "#%06X",
-            0xFFFFFF and ColorUtils.changeBrightness(
-                ColorUtils.getColorFromCode(
-                    context,
-                    rank.chatColorCode,
-                ),
-                1.4f,
+            0xFFFFFF and darkenColor(
+                ContextCompat.getColor(context, colorCodesMap[rank.chatColorCode] ?: 7),
             ),
         )
         val htmlText = TextUtils.replaceColorCodes(
@@ -49,6 +61,13 @@ data class ChatMessage(
             ),
         )
         return HtmlCompat.fromHtml(htmlText, HtmlCompat.FROM_HTML_MODE_LEGACY)
+    }
+
+    private fun darkenColor(color: Int): Int {
+        val hsv = FloatArray(3)
+        Color.colorToHSV(color, hsv)
+        hsv[2] *= 1.4f
+        return Color.HSVToColor(hsv)
     }
 }
 
