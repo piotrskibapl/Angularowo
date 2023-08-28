64974ef6c7d78f8eb9b4ca50985220789279c1cd
piotr.skiba <piotr.skiba@intive.com>
2023-08-28 12:29:10 +0200

    rename ReportBannerData to ReportBanner

--- app/src/main/java/pl/piotrskiba/angularowo/main/report/details/viewmodel/ReportDetailsViewModel.kt
+++ app/src/main/java/pl/piotrskiba/angularowo/main/report/details/viewmodel/ReportDetailsViewModel.kt
@@ -4,13 +4,13 @@ import pl.piotrskiba.angularowo.base.viewmodel.LifecycleViewModel
 import pl.piotrskiba.angularowo.main.report.details.model.ReportData
 import pl.piotrskiba.angularowo.main.report.details.model.toUi
 import pl.piotrskiba.angularowo.main.report.details.ui.ReportDetailsFragmentArgs
-import pl.piotrskiba.angularowo.main.report.model.ReportBannerData
-import pl.piotrskiba.angularowo.main.report.model.toReportBannerData
+import pl.piotrskiba.angularowo.main.report.model.ReportBanner
+import pl.piotrskiba.angularowo.main.report.model.toReportBanner
 import javax.inject.Inject
 
 class ReportDetailsViewModel @Inject constructor() : LifecycleViewModel() {
 
     lateinit var args: ReportDetailsFragmentArgs
-    val reportBanner: ReportBannerData by lazy { args.report.toReportBannerData() }
+    val reportBanner: ReportBanner by lazy { args.report.toReportBanner() }
     val reportDetails: ReportData by lazy { args.report.toUi() }
 }
