5d2f8242df2fc5f6786b52aed11bb0ae35fc147e
Piotr Skiba <pskiba37@gmail.com>
2023-10-19 23:47:59 +0200

    allow dismissing applock if the user has permission

--- app/src/main/java/pl/piotrskiba/angularowo/applock/ui/AppLockFragment.kt
+++ app/src/main/java/pl/piotrskiba/angularowo/applock/ui/AppLockFragment.kt
@@ -4,11 +4,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.navigation.fragment.findNavController
+import pl.piotrskiba.angularowo.R
+import pl.piotrskiba.angularowo.applock.nav.AppLockNavigator
 import pl.piotrskiba.angularowo.applock.viewmodel.AppLockViewModel
+import pl.piotrskiba.angularowo.base.extensions.changeStartDestination
 import pl.piotrskiba.angularowo.base.ui.BaseFragment
 import pl.piotrskiba.angularowo.databinding.FragmentAppLockBinding
 
-class AppLockFragment : BaseFragment<AppLockViewModel>(AppLockViewModel::class) {
+class AppLockFragment : BaseFragment<AppLockViewModel>(AppLockViewModel::class), AppLockNavigator {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        viewModel.navigator = this
+        super.onCreate(savedInstanceState)
+        requireActivity().onBackPressedDispatcher.addCallback(
+            this,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    viewModel.onBackPressed()
+                }
+            },
+        )
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -16,6 +34,14 @@ class AppLockFragment : BaseFragment<AppLockViewModel>(AppLockViewModel::class) 
         savedInstanceState: Bundle?,
     ): View {
         return setupBinding(inflater, container).root
+    }
+
+    override fun navigateToMainScreen() {
+        findNavController().changeStartDestination(R.id.mainScreenFragment)
+    }
+
+    override fun closeApp() {
+        requireActivity().finishAffinity()
     }
 
     private fun setupBinding(
