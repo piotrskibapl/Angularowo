017ff6c370b03d71f9518d8a45224ca062c0bb36
Piotr Skiba <pskiba37@gmail.com>
2024-08-08 15:20:07 +0200

    use new usecase in GetMainScreenDataAndSavePlayerUseCase

--- domain/src/main/java/pl/piotrskiba/angularowo/domain/mainscreen/usecase/GetMainScreenDataAndSavePlayerUseCase.kt
+++ domain/src/main/java/pl/piotrskiba/angularowo/domain/mainscreen/usecase/GetMainScreenDataAndSavePlayerUseCase.kt
@@ -1,37 +1,30 @@
 package pl.piotrskiba.angularowo.domain.mainscreen.usecase
 
-import io.reactivex.rxjava3.core.Maybe
 import io.reactivex.rxjava3.core.Single
 import pl.piotrskiba.angularowo.domain.base.preferences.repository.PreferencesRepository
 import pl.piotrskiba.angularowo.domain.mainscreen.model.MainScreenDataModel
-import pl.piotrskiba.angularowo.domain.player.repository.PlayerRepository
+import pl.piotrskiba.angularowo.domain.player.model.DetailedPlayerModel
+import pl.piotrskiba.angularowo.domain.player.usecase.GetAppUserPlayerUseCase
 import pl.piotrskiba.angularowo.domain.punishment.model.PunishmentFilterModel
 import pl.piotrskiba.angularowo.domain.punishment.model.PunishmentTypeModel
 import pl.piotrskiba.angularowo.domain.punishment.repository.PunishmentRepository
-import pl.piotrskiba.angularowo.domain.rank.repository.RankRepository
 import pl.piotrskiba.angularowo.domain.server.repository.ServerRepository
 import javax.inject.Inject
 
 class GetMainScreenDataAndSavePlayerUseCase @Inject constructor(
     private val serverRepository: ServerRepository,
-    private val playerRepository: PlayerRepository,
-    private val rankRepository: RankRepository,
     private val punishmentRepository: PunishmentRepository,
     private val preferencesRepository: PreferencesRepository,
+    private val getAppUserPlayerUseCase: GetAppUserPlayerUseCase,
 ) {
 
     fun execute(): Single<MainScreenDataModel> =
-        Maybe.zip(
-            preferencesRepository.uuid(),
-            preferencesRepository.username(),
-            ::Pair,
-        )
-            .toSingle()
-            .flatMap { (uuid, username) ->
+        getAppUserPlayerUseCase.execute(ignoreCache = true)
+            .flatMap { player ->
                 Single.zip(
                     getServerStatus(),
-                    getAndSavePlayerData(uuid),
-                    getPlayerPunishments(username),
+                    savePlayerData(player),
+                    getPlayerPunishments(player.username),
                     ::Triple,
                 ).map { (serverStatus, playerData, playerPunishments) ->
                     MainScreenDataModel(serverStatus, playerData, playerPunishments)
@@ -41,18 +34,10 @@ class GetMainScreenDataAndSavePlayerUseCase @Inject constructor(
     private fun getServerStatus() =
         serverRepository.getServerStatus()
 
-    private fun getAndSavePlayerData(uuid: String) =
-        playerRepository.getPlayerDetailsFromUuid(uuid)
-            .flatMap { player ->
-                preferencesRepository.setUsername(player.username)
-                    .mergeWith(preferencesRepository.setRankName(player.rank.name))
-                    .andThen(rankRepository.getAllRanks())
-                    .map { ranks ->
-                        val playerRank =
-                            ranks.firstOrNull { it.name == player.rank.name } ?: player.rank
-                        player.copy(rank = playerRank)
-                    }
-            }
+    private fun savePlayerData(player: DetailedPlayerModel) =
+        preferencesRepository.setUsername(player.username)
+            .mergeWith(preferencesRepository.setRankName(player.rank.name))
+            .toSingleDefault(player)
 
     private fun getPlayerPunishments(username: String) =
         punishmentRepository.getPlayerPunishments(
